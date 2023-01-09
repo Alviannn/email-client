fb4ede57adff7484ac92fd44ea31e0b054ee207f
Alviannn <alvianndq@gmail.com>
2023-01-09 21:45:49 +0700

    feat(db): add database connection and library

---
+++ src/dev/gamavi/emailclient/shared/Shared.java
@@ -0,0 +1,29 @@
+package dev.gamavi.emailclient.shared;
+
+import dev.gamavi.emailclient.repository.UserRepository;
+
+public class Shared {
+	
+	private static Shared instance;
+	
+	public static Shared getInstance() {
+		if (instance == null) {
+			instance = new Shared();
+		}
+		
+		return instance;
+	}
+	
+	private final SQLHelper helper;
+	private final UserRepository userRepo;
+	
+	private Shared() {
+		this.helper = new SQLHelper("localhost", 3306, "email_client", "root", "");
+		this.userRepo = new UserRepository(helper);
+	}
+
+	public SQLHelper getHelper() {
+		return helper;
+	}
+
+}

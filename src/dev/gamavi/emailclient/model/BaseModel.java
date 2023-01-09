9ee9c52c4dbe655123263eff6bf9b30bf3685db0
Alviannn <alvianndq@gmail.com>
2023-01-09 23:13:06 +0700

    feat(basemodel): remove  field

--- src/dev/gamavi/emailclient/model/BaseModel.java
+++ src/dev/gamavi/emailclient/model/BaseModel.java
@@ -3,33 +3,24 @@ package dev.gamavi.emailclient.model;
 import java.sql.Timestamp;
 
 public abstract class BaseModel {
-	
+
 	private Timestamp createdAt;
 	private Timestamp updatedAt;
-	private Timestamp deletedAt;
 
 	public Timestamp getCreatedAt() {
 		return createdAt;
 	}
-	
+
 	public Timestamp getUpdatedAt() {
 		return updatedAt;
 	}
-	
-	public Timestamp getDeletedAt() {
-		return deletedAt;
-	}
-	
+
 	public void setCreatedAt(Timestamp createdAt) {
 		this.createdAt = createdAt;
 	}
-	
+
 	public void setUpdatedAt(Timestamp updatedAt) {
 		this.updatedAt = updatedAt;
 	}
-	
-	public void setDeletedAt(Timestamp deletedAt) {
-		this.deletedAt = deletedAt;
-	}
-	
+
 }

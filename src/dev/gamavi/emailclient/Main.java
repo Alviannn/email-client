0ae4befbcc9a72096b760b9b864037c2ac29b4c3
Alviannn <alvianndq@gmail.com>
2023-01-10 20:07:31 +0700

    chore: add vscode settings

--- src/dev/gamavi/emailclient/Main.java
+++ src/dev/gamavi/emailclient/Main.java
@@ -1,15 +1,16 @@
 package dev.gamavi.emailclient;
 
 import dev.gamavi.emailclient.menu.MainMenu;
-import dev.gamavi.emailclient.menu.RegisterMenu;
 
 public class Main {
 
 	public Main() {
+		
+
 		MainMenu mainMenu = new MainMenu();
 		mainMenu.show();
 	}
-	
+
 	public static void main(String[] args) {
 		new Main();
 	}
